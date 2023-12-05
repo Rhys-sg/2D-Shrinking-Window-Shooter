2750190e386d81458a4370309c8f10354f2278eb
Rhys Sorenson-Graff <sorensor@whitman.edu>
2023-12-05 15:15:38 -0800

    Update start and end screen - not finished

--- GameWindow.py
+++ GameWindow.py
@@ -27,11 +27,57 @@ class GameWindow(tk.Tk):
         y = (screen_height - self.height) // 2
         self.geometry(f"{self.width}x{self.height}+{x}+{y}")
 
-        self.start_button = tk.Button(self, text="Start", command=self.start_game)
-        self.start_button.pack(pady=self.height / 2.5)
 
         self.canvas = tk.Canvas(self, width=self.width, height=self.height)
         self.canvas.pack()
+
+        # Draw the rectangle for the button
+        button_width = 100
+        button_height = 40
+        x_padding = 10
+        y_padding = 10
+        title_width = (button_width * 2) + x_padding
+        title_height = 75
+
+        # photo = tk.PhotoImage(file="C:/Users/rsore/Documents/GitHub/2D-Shrinking-Window-Shooter/Assets/Icon_nobg.png")
+
+        # image_x = (self.width - photo.width()) // 2
+        # image_y = self.height // 5
+        # title = self.canvas.create_image(image_x, image_y, anchor=tk.N, image=photo)
+        # self.canvas.itemconfig(title, state=tk.NORMAL)
+
+        title_button_x = ((self.width - button_width) // 2) - button_width // 2 - x_padding // 2
+        title_button_y = self.height // 2.5 - title_height - y_padding
+        self.canvas.create_rectangle(title_button_x, title_button_y, title_button_x + title_width, title_button_y + title_height, outline="black", tags="title_button")
+        title_text_x = title_button_x + title_width // 2
+        title_text_y = title_button_y + title_height // 2
+        self.canvas.create_text(title_text_x, title_text_y, text="CLAUSTROPHOBIA", font=("Helvetica", 12, "bold"), tags="title_text")
+
+        start_button_x = ((self.width - button_width) // 2) - button_width // 2 - x_padding // 2
+        start_button_y = self.height // 2.5
+        self.canvas.create_rectangle(start_button_x, start_button_y, start_button_x + button_width, start_button_y + button_height, outline="black", tags="start_button")
+        start_text_x = start_button_x + button_width // 2
+        start_text_y = start_button_y + button_height // 2
+        self.canvas.create_text(start_text_x, start_text_y, text="Start", font=("Helvetica", 12, "bold"), tags="start_text")
+        self.canvas.tag_bind("start_button", "<Button-1>", lambda event: self.start_game())
+        self.canvas.tag_bind("start_text", "<Button-1>", lambda event: self.start_game())
+        self.canvas.tag_bind("start_button", "<Enter>", lambda event: self.canvas.itemconfig("start_button", fill="lightgrey"))
+        self.canvas.tag_bind("start_button", "<Leave>", lambda event: self.canvas.itemconfig("start_button", fill=""))
+        self.canvas.tag_bind("start_text", "<Enter>", lambda event: self.canvas.itemconfig("start_button", fill="lightgrey"))
+        self.canvas.tag_bind("start_text", "<Leave>", lambda event: self.canvas.itemconfig("start_button", fill=""))
+
+        help_button_x = ((self.width - button_width) // 2) + button_width // 2 + x_padding // 2
+        help_button_y = self.height // 2.5
+        self.canvas.create_rectangle(help_button_x, help_button_y, help_button_x + button_width, help_button_y + button_height, outline="black", tags="help_button")
+        self.help_text_x = help_button_x + button_width // 2
+        self.help_text_y = help_button_y + button_height // 2
+        self.canvas.create_text(self.help_text_x, self.help_text_y, text="Help", font=("Helvetica", 12, "bold"), tags="help_text")
+        self.canvas.tag_bind("help_button", "<Button-1>", lambda event: self.start_game())
+        self.canvas.tag_bind("help_text", "<Button-1>", lambda event: self.start_game())
+        self.canvas.tag_bind("help_button", "<Enter>", lambda event: self.canvas.itemconfig("help_button", fill="lightgrey"))
+        self.canvas.tag_bind("help_button", "<Leave>", lambda event: self.canvas.itemconfig("help_button", fill=""))
+        self.canvas.tag_bind("help_text", "<Enter>", lambda event: self.canvas.itemconfig("help_button", fill="lightgrey"))
+        self.canvas.tag_bind("help_text", "<Leave>", lambda event: self.canvas.itemconfig("help_button", fill=""))
 
         self.sprite_size = 20
         self.sprite = Sprite.Sprite(self.canvas, self.width, self.height, self.sprite_size)
@@ -63,9 +109,13 @@ class GameWindow(tk.Tk):
         self.last_key = ""
           
 
-
     def start_game(self):
-        self.start_button.pack_forget()
+        self.canvas.itemconfig("title_button", state=tk.HIDDEN)
+        self.canvas.itemconfig("title_text", text="")
+        self.canvas.itemconfig("start_button", state=tk.HIDDEN)
+        self.canvas.itemconfig("start_text", text="")
+        self.canvas.itemconfig("help_button", state=tk.HIDDEN)
+        self.canvas.itemconfig("help_text", text="")
 
         # Show the sprite when the game starts
         self.sprite.show()
@@ -158,10 +208,12 @@ class GameWindow(tk.Tk):
 
         # Display game over message
         game_over_label = tk.Label(self, text="Game Over", font=("Helvetica", 24))
-        game_over_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        game_over_label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
-        score_label = tk.Label(self, text=f"Score: {self.score}", font=("Helvetica", 12))
-        score_label.place(relx=0.5, rely=0.575, anchor=tk.CENTER)
+        self.canvas.itemconfig("start_text", text="Restart")
+        self.canvas.itemconfig("start_button", state=tk.NORMAL)
+        
+        self.canvas.itemconfig("help_text", text=f"Score: {self.score}")
 
         for enemy in self.enemies:
             enemy.delete_enemy()
